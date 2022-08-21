c28e406938830551698a78631f8d7b7c82ea648a
adityor6 <adityor6@gmail.com>
2022-08-22 00:15:52 +0700

    first commit

--- main.go
+++ main.go
@@ -2,10 +2,5 @@ package main
 import "fmt"
 func main() {
 	fmt.Println("Hello world")
-<<<<<<< HEAD
-	fmt.Println("A")
-	fmt.Println("B")
-	fmt.Println("feature A")
-=======
->>>>>>> 04c5a4951766a8a1b12d799871acdc9a5e530a36
+
 }
